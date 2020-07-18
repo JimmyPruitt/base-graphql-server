c97677573cb49739f8b892f5e4c393dbe467c983
Jimmy Pruitt <jimmy.pruitt@outlook.com>
2020-07-18 15:51:59 -0700

    chore: include TSX files in check for tsc on commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.{ts,tsx,js,jsx}': ['yarn lint'],
   '**/*.{ts,tsx,js,jsx,json,gql,md,html,css,less,scss}': ['yarn prettier'],
   '*': files => {
-    if (Array.isArray(files) && files.some(file => file.endsWith('.ts'))) {
+    if (Array.isArray(files) && files.some(file => !!file.match(/\.tsx?$/))) {
       return ['yarn build']
     }
 
