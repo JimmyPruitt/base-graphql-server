71704366bbf09288d2c04ed3368f293279dc8883
Jimmy Pruitt <jimmy.pruitt@outlook.com>
2020-07-18 15:36:48 -0700

    chore: make linter and prettier run more selectively

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  '*': () => ['yarn prettier', 'yarn lint', 'yarn build']
+  '**/*.{ts,tsx,js,jsx}': ["yarn lint"],
+  '**/*.{ts,tsx,js,jsx,json,gql,md,html,css,less,scss': ["yarn prettier"],
+  '*': (files) => {
+    if (Array.isArray(files) && files.some(file => file.endsWith('.ts'))) {
+      return ['yarn build']
+    }
+
+    return [];
+  },
 }
