0c18b1fd72dbf51c0bb2ff956a350c1b4140c5f8
Jimmy Pruitt <jimmy.pruitt@outlook.com>
2020-07-18 15:42:24 -0700

    fix: replace missing closing brace in .lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  '**/*.{ts,tsx,js,jsx}': ["yarn lint"],
-  '**/*.{ts,tsx,js,jsx,json,gql,md,html,css,less,scss': ["yarn prettier"],
-  '*': (files) => {
+  '**/*.{ts,tsx,js,jsx}': ['yarn lint'],
+  '**/*.{ts,tsx,js,jsx,json,gql,md,html,css,less,scss}': ['yarn prettier'],
+  '*': files => {
     if (Array.isArray(files) && files.some(file => file.endsWith('.ts'))) {
       return ['yarn build']
     }
 
-    return [];
-  },
+    return []
+  }
 }
